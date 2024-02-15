5efebc9f753e24878411cd333a423dbc4d3ff4a8
Lucas Lourenço <lucascelestiano@gmail.com>
2024-02-15 15:10:38 -0400

    refactor: update status code

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -1,10 +1,11 @@
 export enum StatusCode {
   "CRIADO" = 10,
-  "Erro no banco/conta" = 38,
   "PROCESSADO" = 35,
   "PAGO" = 32,
-  "Erro ao processar" = 34,
   "CANCELADO" = 31,
+  "Erro no banco/conta" = 38,
+  "Erro ao processar pagamento" = 34,
   "Reembolso aprovado" = 3,
-  "Erro na devolução" = 4,
+  "Erro no reembolso" = 4,
+  "PAGO (webhook)" = 11,
 }
